a1f200f5285ee0421b68734070a21755150352ed
Novonil Basakk <novoobasak@gmail.com>
2025-10-13 06:54:06 +0530

    Fix: Remove getAllTestimonials function

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,4 @@
-import { Project, Testimonial } from '@/lib/types'
+import { Project } from '@/lib/types'
 import { promises as fs } from 'fs'
 import path from 'path'
 
@@ -33,28 +33,4 @@ const getAllProjects = async (): Promise<Project[]> => {
   }
 }
 
-const getAllTestimonials = async (): Promise<Testimonial[]> => {
-  try {
-    const testimonialsPath = path.join(process.cwd(), '/content/testimonials')
-    const testimonialsName = await fs.readdir(testimonialsPath)
-
-    const testimonials = await Promise.all(
-      testimonialsName.map(async (projectName) => {
-        const filePath = path.join(testimonialsPath, projectName)
-        const projectDetails = await fs.readFile(filePath, 'utf8')
-        return JSON.parse(projectDetails)
-      }),
-    )
-
-    // Sort testimonials by date
-    testimonials.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
-
-    return testimonials
-  } catch (error) {
-    // Handle errors
-    console.error('Error:', error)
-    return []
-  }
-}
-
-export { getAllProjects, getAllTestimonials }
+export { getAllProjects }
